d76e447bb00f028ef513797f94847ae67745d7a2
Lucas <lucasguimaraes.reis@estudante.ufjf.br>
2025-11-06 12:24:49 -0300

    :sparkles: adicionei fontes

--- src/Componentes/atributo.tsx
+++ src/Componentes/atributo.tsx
@@ -9,10 +9,13 @@ export default function Atributo({
     const estilos: React.CSSProperties = {
         fontSize: 'clamp(5px, 1vw , 11px)',
     };
+    
+    const valorLimitado = Math.min(valor, 5000);
+    
     return (
         <div className={`${tipo}`} style = {estilos}>
-            {tipo === 'ataque' ? 'ATK/' : tipo === 'defesa' ? ' DEF/' : ''} 
-            <span style={estilos}>{valor}</span>
+           <span style={{fontWeight:'700'}}>{tipo === 'ataque' ? 'ATK/' : tipo === 'defesa' ? ' DEF/' : ''} </span> 
+            <span style={estilos}>{valorLimitado}</span>
         </div>
     )
 }      
